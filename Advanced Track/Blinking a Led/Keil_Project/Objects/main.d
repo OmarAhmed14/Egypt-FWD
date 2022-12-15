0f1224a53b0f0aec799ed21adef527e6ed36295f
Omar Ahmed <Oa0578222@gmail.com>
2022-12-15 20:21:03 +0200

    Advanced Track

--- Advanced Track/Blinking a Led/Keil_Project/Objects/main.d
+++ Advanced Track/Blinking a Led/Keil_Project/Objects/main.d
@@ -1,3 +1,1 @@
-./objects/main.o: main.c Std_Types.h Bit_Math.h \
-  COTS\MCAL\DIO\DIO_Interface.h COTS\MCAL\IntCtrl\IntCtrl_Cfg.h \
-  COTS\MCAL\IntCtrl\IntCtrl_Interface.h GPTM_Interface.h GPTM_CFG.h
+./objects/main.o: main.c Std_Types.h APP.h
--- Advanced Track/Blinking a Led/Keil_Project/main.c
+++ Advanced Track/Blinking a Led/Keil_Project/main.c
@@ -1,44 +1,20 @@
 #include "Std_Types.h"
-#include "Bit_Math.h"
+#include "APP.h"
 
-#include "DIO_Interface.h"
-
-#include "IntCtrl_Cfg.h"
-#include "IntCtrl_Interface.h"
-
-#include "GPTM_Interface.h"
-#include "GPTM_CFG.h"
-
-void LED_Blinking (void);
-
-u8 flag;
+u8 on;
+u8 off;
 
 int main ()
 {	
-	flag=0;
-	IntCtrl_Init();
-	GPTM_CallBackFn(&LED_Blinking);
-	DIO_DirectionChannel(DIO_PORTF,PIN1,OUTPUT_PIN);
-	GPTM_Init();
+	/*Initial Values for on/off Time*/
+	on=1;
+	off=3;
+	
+	APP_Init();
 	
 	while (1)
 	{
-		/*No thing here to save Power*/
+		APP_Start (off,on);
 	}
 }
 
-/*******************ISR To Toggle LED every 1 sec*********************/
-
-void LED_Blinking (void)
-{
-	if (!flag)
-	{
-		DIO_WriteChannel(DIO_PORTF,PIN1,HIGH);
-		flag=1;
-	}
-	else
-	{
-		DIO_WriteChannel(DIO_PORTF,PIN1,LOW);
-		flag=0;
-	}
-}
